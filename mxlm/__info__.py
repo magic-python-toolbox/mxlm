76a27a256d4182fbaf09103d4589613b72fce238
Lei <ylxx@live.com>
2024-03-30 16:39:33 +0800

    init

--- mxlm/__info__.py
+++ mxlm/__info__.py
@@ -1,13 +1,13 @@
 __version__ = "0.0.1"
-__description__ = "mxlm"
+__description__ = "Language Model Utils"
 __license__ = "MIT"
 __author__ = "DIYer22"
 __author_email__ = "ylxx@live.com"
 __maintainer__ = "DIYer22"
 __maintainer_email__ = "ylxx@live.com"
 __github_username__ = "DIYer22"
-__url__ = "https://github.com/DIYer22/mxlm"
-__support__ = "https://github.com/DIYer22/mxlm/issues"
+__url__ = "https://github.com/magic-python-toolbox/mxlm"
+__support__ = "https://github.com/magic-python-toolbox/mxlm/issues"
 __classifiers__ = [
     "Topic :: Software Development :: Libraries :: Python Modules",
     "Programming Language :: Python :: 3",
