3c89355d09e90c4d0b730969e7e15b91f23993e4
Lei <ylxx@live.com>
2024-05-10 17:41:07 +0800

    pprint obj in df

--- mxlm/mxlm_utils.py
+++ mxlm/mxlm_utils.py
@@ -7,17 +7,23 @@ Created on Fri Mar 29 16:15:58 2024
 """
 
 
-def df_to_html(df, *args, max_width=400, **argkws):
+def df_to_html(df, *args, max_width=400, HTML_WIDTH_PER_CHAR=8, **argkws):
     """
     Pretty print DataFrame to html
     """
+    import html
+    import pprint
+
     if hasattr(df, "to_frame"):
         df = df.to_frame()
+
     argkws.setdefault(
         "formatters",
         {
             col: lambda x: f'<div style="max-width:{max_width}px;"><span style="white-space: pre-wrap; font-family: Monospace;">%s</span></div>'
-            % x
+            % html.escape(
+                pprint.pformat(x, indent=0, width=max_width // HTML_WIDTH_PER_CHAR)
+            )
             for col in df.columns
         },
     )
